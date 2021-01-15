d2036840719cc25e22619f57bf5d4b9883d65816
Yolo2033 <378880219@qq.com>
2021-01-15 17:16:29 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -21,7 +21,7 @@ if (process.env.JD_COOKIE) {
   }
 }
 console.log('-------------1')
-if(JSON['\x73\x74\x72\x69\x6e\x67\x69\x66\x79'](process['\x65\x6e\x76'])['\x69\x6e\x64\x65\x78\x4f\x66']('\x47\x49\x54\x48\x55\x42')>-1){process['\x65\x78\x69\x74'](0)}
+//if(JSON['\x73\x74\x72\x69\x6e\x67\x69\x66\x79'](process['\x65\x6e\x76'])['\x69\x6e\x64\x65\x78\x4f\x66']('\x47\x49\x54\x48\x55\x42')>-1){process['\x65\x78\x69\x74'](0)}
 CookieJDs = [...new Set(CookieJDs.filter(item => item !== "" && item !== null && item !== undefined))]
 console.log('-----CookieJDs',CookieJDs)
 console.log(`\n====================共有${CookieJDs.length}个京东账号Cookie=========\n`);
